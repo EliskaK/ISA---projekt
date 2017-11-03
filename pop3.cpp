99ae1a6065d69f31695c10ef039090907638c013
Unknown <elis.kadlecova@gmail.com>
2017-11-04 00:46:56 +0100

    nove prikazy pop3
    messageList, downloadMsg, del, top, retr, stat; doplneny nektere private promenne pro tridu pop3

--- pop3.cpp
+++ pop3.cpp
@@ -9,7 +9,12 @@
 #include "main.hpp"
 
 POP3::POP3(){ //konstruktor
-std::cout << "konstruktor" << '\n';
+//std::cout << "konstruktor" << '\n';
+}
+
+POP3::~POP3(){
+
+
 }
 
 /*
@@ -27,12 +32,12 @@ bool POP3::connect_server(std::string server, int port){
   struct hostent *host;
   struct sockaddr_in server_addr;
   struct in_addr **addresses;
-  std::cout << "server: " << server << '\n';
+  //std::cout << "server: " << server << '\n';
   host = gethostbyname(server.c_str());
   if (!host){
     error("Server se nepodarilo nacist", 4);
   }
-  std::cout << "host: " << host << '\n';
+  //std::cout << "host: " << host << '\n';
   addresses = (struct in_addr **) host->h_addr_list;
   for(int i = 0; addresses[i] != NULL; i++) {
     server_addr.sin_addr = *addresses[i];
@@ -40,7 +45,7 @@ bool POP3::connect_server(std::string server, int port){
   }
 
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  std::cout << "sockfd: " << sockfd << '\n';
+  //std::cout << "sockfd: " << sockfd << '\n';
   if (sockfd == -1){
     error("Nepodarilo se vytvorit socket", 4);
   }
@@ -49,11 +54,13 @@ bool POP3::connect_server(std::string server, int port){
   server_addr.sin_port = htons((uint16_t)port);
   memcpy(&(server_addr.sin_addr), host->h_addr, host->h_length);
   int a = connect(sockfd, (sockaddr*) &server_addr, sizeof(server_addr));
-  std::cout << "a: "<< a << '\n';
+  //std::cout << "a: "<< a << '\n';
   if(a < 0){
     error("Nepodarilo se pripojit k serveru", 4);
   }
   sock = sockfd;
+  get_response();
+  printf("S: %s",buff);
   return true; //connect se podarilo
 }
 
@@ -63,14 +70,17 @@ bool POP3::connect_server(std::string server, int port){
 bool POP3::login(std::string username, std::string password){
   std::string sending_user = "USER ";
   sending_user += username;
-  std::cout << sending_user << '\n';
+
   send_command(sending_user);
   get_response();
+
+  printf("S: %s",buff);
   std::string sending_pass = "PASS ";
   sending_pass += password;
-  std::cout << sending_pass << '\n';
+
   send_command(sending_pass);
   get_response();
+  printf("S: %s",buff);
 
   return true;
 }
@@ -79,11 +89,81 @@ bool POP3::login(std::string username, std::string password){
 * Posle prikaz LOGOUT
 */
 bool POP3::logout(){
+  send_command("QUIT");
+  get_response();
+  printf("S: %s",buff);
   return true;
 }
 
+/*
+* Posle prikaz LIST k vypsani poctu zprav a jejich velikosti
+*/
+void POP3::messageList (bool new_only, std::string out_dir){
+  send_command("STAT");
+  get_response();
+  printf("S: %s",buff);
+}
+
+bool POP3::stat (){
+  send_command("STAT");
+  get_response();
+  printf("S: %s",buff);
+  char* temp = strstr(buff, "+OK ");
+  if (temp == NULL){
+    return false;
+  }
+  else{
+    std::cout << "temp:" << temp << '\n';
+    sscanf(temp, "%d", &numMsg);
+    //TODO: vypreparovat z temp jen pocet zprav
+    std::cout << "numMsg: " << numMsg <<'\n';
+    return true;
+  }
+}
+
+void POP3::del (){
+  send_command("DELE 1");
+  get_response();
+  printf("S: %s",buff);
+}
+
+void POP3::top (){
+  send_command("TOP 2 100");
+  get_response();
+  printf("S: %s",buff);
+}
+
+void POP3::retr (){
+  send_command("RETR 2");
+  //char buff[2048] = {0};
+  get_response();
+  printf("S: %s",buff);
+}
+
+bool POP3::downloadMsg(std::string out_dir){
+  char *temp;
+  temp = new char[out_dir.length() + 1];
+  strcpy(temp, out_dir.c_str());
+  DIR *dir;
+  dir = opendir(temp);
+  if(dir){
+    closedir(dir);
+  }
+  else {
+    error("Otevreni slozky pro vystup se nezdrailo", 5);
+  }
+  stat();
+
+
+  return true;
+}
+
+/*
+* Posle dany prikaz na server
+*/
 bool POP3::send_command(std::string command){
   command = command.append("\r\n");
+  std::cout << "C: " << command;
   int n = write(sock, command.c_str(), command.length());
   if(n < 0){
     error("Nepodarilo se poslat prikaz na server", 5);
@@ -91,12 +171,25 @@ bool POP3::send_command(std::string command){
   return true;
 }
 
-bool POP3::get_response(){
-  char buff[201];
-  int n = read(sock, buff, 200);
+/*
+* Precte odpoved ze serveru
+*/
+ bool POP3::get_response(){
+  memset(buff, 0, 2048); // vyprazdneni bufferu
+  int n;
+  n = read(sock, buff, sizeof(buff));
   if(n < 0){
-    error("Nepodarilo se ziskat odpoved serveru", 5);
+   error("Nepodarilo se ziskat odpoved serveru", 5);
   }
-  printf("%s\n",buff);
+  // while((n = read(sock, buff, sizeof(buff))) > 0){
+  //   printf("S: %s",buff);
+  // }
+  // if(n < 0){
+  //   error("Nepodarilo se ziskat odpoved serveru", 5);
+  // }
+
+
+
+  //std::cout << buff << "\n";
   return true;
 }
--- pop3.hpp
+++ pop3.hpp
@@ -13,19 +13,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
 
 //#include "main.hpp"
 
 class POP3{
   public:
     POP3();
+    ~POP3();
     bool connect_server(std::string server, int port);
     bool login(std::string username, std::string password);
     bool logout();
     bool send_command(std::string command);
     bool get_response();
+    void messageList (bool new_only, std::string out_dir);
+    bool downloadMsg(std::string out_dir);
+    void del();
+    void top ();
+    void retr ();
+    bool stat ();
   private:
     BIO *bio;
     SSL_CTX *ctx;
     int sock;
+    std::string message;
+    char buff[2048] = {0}; //inicializace na nulu
+    int numMsg; //pocet zprav
 };
